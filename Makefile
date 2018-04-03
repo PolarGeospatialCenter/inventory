1bd50ade7b371509f0018d7d9a4e3824995bc864
Andrew Zenk <azenk@umn.edu>
2018-04-03 16:02:42 -0500

    Refactor to use dockertest for dynamodb testing

--- Makefile
+++ Makefile
@@ -3,6 +3,8 @@ test: deps
 
 deps: vendor
 	go get github.com/hashicorp/consul/api
+	go get github.com/aws/aws-sdk-go/aws
+	go get github.com/aws/aws-sdk-go/service
 
 vendor: Gopkg.toml Gopkg.lock
 	dep ensure
--- pkg/inventory/dynamodbstore_test.go
+++ pkg/inventory/dynamodbstore_test.go
@@ -3,75 +3,18 @@ package inventory
 import (
 	"context"
 	"fmt"
-	"log"
 	"os"
 	"path/filepath"
 	"testing"
 
-	"docker.io/go-docker"
-	dockertypes "docker.io/go-docker/api/types"
-	"docker.io/go-docker/api/types/container"
-	"docker.io/go-docker/api/types/network"
+	dynamodbtest "github.com/PolarGeospatialCenter/dockertest/pkg/dynamodb"
 	"github.com/PolarGeospatialCenter/inventory/pkg/inventory/types"
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/go-test/deep"
 	git "gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/storage/memory"
 )
-
-func startDynamoDB(ctx context.Context) (*dynamodb.DynamoDB, error) {
-	cli, err := docker.NewEnvClient()
-	if err != nil {
-		panic(err)
-	}
-
-	containerConfig := &container.Config{
-		Image: "deangiberson/aws-dynamodb-local",
-	}
-
-	hostConfig := &container.HostConfig{
-		PublishAllPorts: true,
-	}
-
-	networkConfig := &network.NetworkingConfig{}
-
-	c, err := cli.ContainerCreate(ctx, containerConfig, hostConfig, networkConfig, "")
-	if err != nil {
-		return nil, err
-	}
-
-	go func() {
-		<-ctx.Done()
-		log.Print("killing dynamodb container")
-		err := cli.ContainerKill(context.Background(), c.ID, "SIGINT")
-		if err != nil {
-			log.Printf("Error killing dynamodb container %s: %v", c.ID, err)
-		}
-		cli.ContainerWait(context.Background(), c.ID, container.WaitConditionNotRunning)
-		cli.ContainerRemove(context.Background(), c.ID, dockertypes.ContainerRemoveOptions{})
-	}()
-
-	err = cli.ContainerStart(ctx, c.ID, dockertypes.ContainerStartOptions{})
-	if err != nil {
-		return nil, err
-	}
-
-	log.Print(c.ID)
-	containerData, err := cli.ContainerInspect(ctx, c.ID)
-	if err != nil {
-		return nil, err
-	}
-
-	ports := containerData.NetworkSettings.Ports
-	log.Print(ports)
-	port := ports["8000/tcp"][0].HostPort
-	endpoint := fmt.Sprintf("http://localhost:%s", port)
-	region := "us-east-2"
-	db := dynamodb.New(session.New(&aws.Config{Endpoint: &endpoint, Region: &region}))
-	return db, nil
-}
 
 func loadGitStore() *GitStore {
 	path, _ := os.Getwd()
@@ -84,30 +27,15 @@ func loadGitStore() *GitStore {
 	return NewGitStore(repo, &git.FetchOptions{}, "master")
 }
 
-func TestDynamoDB(t *testing.T) {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	db, err := startDynamoDB(ctx)
-	if err != nil {
-		t.Errorf("unable to start dynamodb: %v", err)
-	}
-
-	out, err := db.ListTables(&dynamodb.ListTablesInput{})
-	if err != nil {
-		t.Errorf("Unable to list tables: %v", err)
-	}
-	t.Log(out.TableNames)
-}
-
 func TestDynamoDBUpdate(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
-
-	db, err := startDynamoDB(ctx)
+	dbInstance, err := dynamodbtest.Run(ctx)
 	if err != nil {
 		t.Errorf("unable to start dynamodb: %v", err)
 	}
+	defer dbInstance.Stop(ctx)
+	db := dynamodb.New(session.New(dbInstance.Config()))
 
 	store := loadGitStore()
 	store.Refresh()
