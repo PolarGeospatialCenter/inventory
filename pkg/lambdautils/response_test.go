14d0e417eaf6bccb5277cc427b971fe42613e7c2
Andrew Zenk <azenk@umn.edu>
2018-04-20 11:28:51 -0500

    DRY up lambdautil testing

--- pkg/lambdautils/response_test.go
+++ pkg/lambdautils/response_test.go
@@ -3,36 +3,44 @@ package lambdautils
 import (
 	"net/http"
 	"testing"
+
+	"github.com/go-test/deep"
 )
 
 func TestNewJSONAPIGatewayProxyResponseString(t *testing.T) {
-	response, err := NewJSONAPIGatewayProxyResponse(http.StatusOK, map[string]string{}, "Hello World!")
-	if err != nil {
-		t.Fatalf("Unable to create response object from string: %v", err)
+	type testCase struct {
+		Status       int
+		Headers      map[string]string
+		BodyData     interface{}
+		ExpectedBody string
 	}
 
-	if response.StatusCode != http.StatusOK {
-		t.Errorf("Wrong status code returned: %d", response.StatusCode)
+	cases := []testCase{
+		testCase{http.StatusOK, map[string]string{}, "Hello World!", "Hello World!"},
+		testCase{http.StatusOK, map[string]string{}, struct {
+			Test string `json:"test"`
+		}{Test: "Hello World!"}, `{"test":"Hello World!"}`},
 	}
 
-	if response.Body != "Hello World!" {
-		t.Errorf("Wrong body returned: %s", response.Body)
-	}
-}
+	for _, c := range cases {
+		response, err := NewJSONAPIGatewayProxyResponse(c.Status, c.Headers, c.BodyData)
+		if err != nil {
+			t.Fatalf("Unable to create response object from string: %v", err)
+		}
 
-func TestNewJSONAPIGatewayProxyResponseObject(t *testing.T) {
-	response, err := NewJSONAPIGatewayProxyResponse(http.StatusOK, map[string]string{}, struct {
-		Test string `json:"test"`
-	}{Test: "Hello World!"})
-	if err != nil {
-		t.Fatalf("Unable to create response object from string: %v", err)
-	}
+		if response.StatusCode != c.Status {
+			t.Errorf("Wrong status code returned: got %d; expected %d", response.StatusCode, c.Status)
+		}
 
-	if response.StatusCode != http.StatusOK {
-		t.Errorf("Wrong status code returned: %d", response.StatusCode)
-	}
+		if diff := deep.Equal(response.Headers, c.Headers); len(diff) > 0 {
+			t.Error("header mismatch:")
+			for _, l := range diff {
+				t.Error(l)
+			}
+		}
 
-	if response.Body != `{"test":"Hello World!"}` {
-		t.Errorf("Wrong body returned: %s", response.Body)
+		if response.Body != c.ExpectedBody {
+			t.Errorf("Wrong body returned: got '%s'; expected '%s'", response.Body, c.ExpectedBody)
+		}
 	}
 }
