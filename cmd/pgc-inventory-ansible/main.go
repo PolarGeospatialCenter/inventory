33f54819490ae6495748e0ff525062e4229be552
Andrew Zenk <azenk@umn.edu>
2018-06-13 18:57:11 -0500

    Add inventory plugin for ansible
    Currently it only supports using a file inventory.  That should change
    in the future.

---
+++ cmd/pgc-inventory-ansible/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/PolarGeospatialCenter/inventory/pkg/inventory"
+	"github.com/spf13/viper"
+)
+
+type AnsibleGroup struct {
+	Hosts []string               `json:"hosts"`
+	Vars  map[string]interface{} `json:"vars"`
+}
+
+type AnsibleGroupList map[string]*AnsibleGroup
+
+func (l *AnsibleGroupList) Get(groupname string) *AnsibleGroup {
+	group, ok := (*l)[groupname]
+	if !ok {
+		log.Printf("Group doesn't exist yet, creating: %s", groupname)
+		group = &AnsibleGroup{Hosts: []string{}, Vars: make(map[string]interface{})}
+		(*l)[groupname] = group
+	}
+	log.Print(group)
+	return group
+}
+
+func (g *AnsibleGroup) AddHost(hostname string) {
+	g.Hosts = append(g.Hosts, hostname)
+}
+
+func main() {
+
+	cfg := viper.New()
+	cfg.AddConfigPath(".")
+	cfg.SetConfigName("pgc-inventory")
+	cfg.ReadInConfig()
+
+	log.Printf("Loading filestore from: %s", cfg.GetString("path"))
+	store, err := inventory.NewFileStore(cfg.GetString("path"))
+	if err != nil {
+		log.Fatalf("Unable to create file store: %v", err)
+	}
+
+	nodes, err := store.Nodes()
+	if err != nil {
+		log.Fatalf("Unable to read nodes: %v", err)
+	}
+
+	groups := make(AnsibleGroupList)
+	hostVars := make(map[string]map[string]interface{})
+
+	for _, node := range nodes {
+		domain := node.Networks["provisioning"].Network.Domain
+		fqdn := fmt.Sprintf("%s.%s", node.Hostname, domain)
+		group := groups.Get(node.System.ID())
+		group.AddHost(fqdn)
+		roleGroup := fmt.Sprintf("%s-%s", node.System.ID(), node.Role)
+		group = groups.Get(roleGroup)
+		groups[roleGroup].AddHost(fqdn)
+		hostVars[fqdn] = make(map[string]interface{})
+		hostVars[fqdn]["ansible_python_interpreter"] = "/opt/ansible/bin/python"
+		hostVars[fqdn]["tags"] = node.Tags
+		hostVars[fqdn]["inventory_id"] = node.ID()
+		hostVars[fqdn]["rack"] = node.Location.Rack
+		hostVars[fqdn]["role"] = node.Role
+		hostVars[fqdn]["ansible_host"] = node.Networks["provisioning"].NIC.IP
+		hostVars[fqdn]["last_update"] = node.LastUpdated
+	}
+
+	result := make(map[string]interface{})
+	for gName, group := range groups {
+		result[gName] = group
+	}
+	result["_meta"] = make(map[string]interface{})
+	result["_meta"].(map[string]interface{})["hostvars"] = hostVars
+
+	txt, err := json.Marshal(result)
+	if err != nil {
+		log.Fatalf("Unable to marshal group data: %v", err)
+	}
+	fmt.Printf(string(txt))
+}
