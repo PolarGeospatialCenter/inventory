5768bb75cf05d0a93295750466314e61dda4cdfa
Andrew Zenk <azenk@umn.edu>
2018-06-24 15:29:48 -0500

    Refactor network resource tests to use our test framework

--- handlers/network/main_test.go
+++ handlers/network/main_test.go
@@ -2,19 +2,18 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"net"
 	"net/http"
 	"testing"
 
 	dynamodbtest "github.com/PolarGeospatialCenter/dockertest/pkg/dynamodb"
+	"github.com/PolarGeospatialCenter/inventory/pkg/api/testutils"
 	"github.com/PolarGeospatialCenter/inventory/pkg/inventory"
 	inventorytypes "github.com/PolarGeospatialCenter/inventory/pkg/inventory/types"
 	"github.com/PolarGeospatialCenter/inventory/pkg/lambdautils"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
-	"github.com/go-test/deep"
 )
 
 func TestGetHandler(t *testing.T) {
@@ -49,66 +48,39 @@ func TestGetHandler(t *testing.T) {
 		t.Errorf("unable to create test record: %v", err)
 	}
 
-	type testCase struct {
-		context         context.Context
-		queryParameters map[string]string
-		ExpectedBody    interface{}
-		ExpectedStatus  int
-	}
-
 	handlerCtx := lambdautils.NewAwsConfigContext(ctx, dbInstance.Config())
 
-	cases := []testCase{
-		testCase{handlerCtx, map[string]string{"id": "foo"}, lambdautils.ErrorResponse{Status: "Not Found", ErrorMessage: "Object not found"}, http.StatusNotFound},
-		testCase{handlerCtx, map[string]string{"id": "testnetwork"}, network, http.StatusOK},
-		testCase{handlerCtx, map[string]string{"badparam": "foo"}, lambdautils.ErrorResponse{Status: "Bad Request", ErrorMessage: "invalid request, please check your parameters and try again"}, http.StatusBadRequest},
-		testCase{handlerCtx, map[string]string{}, lambdautils.ErrorResponse{Status: "Bad Request", ErrorMessage: "No network requested, please add query parameters"}, http.StatusBadRequest},
+	cases := testutils.TestCases{
+		testutils.TestCase{Ctx: handlerCtx,
+			Request: events.APIGatewayProxyRequest{
+				HTTPMethod:            http.MethodGet,
+				QueryStringParameters: map[string]string{"id": "foo"},
+			},
+			TestResult: testutils.ExpectError(http.StatusNotFound, "Object not found"),
+		},
+		testutils.TestCase{Ctx: handlerCtx,
+			Request: events.APIGatewayProxyRequest{
+				HTTPMethod:            http.MethodGet,
+				QueryStringParameters: map[string]string{"id": "testnetwork"},
+			},
+			TestResult: &testutils.TestResult{
+				ExpectedBodyObject: network,
+				ExpectedStatus:     http.StatusOK,
+			},
+		},
+		testutils.TestCase{Ctx: handlerCtx,
+			Request: events.APIGatewayProxyRequest{
+				HTTPMethod:            http.MethodGet,
+				QueryStringParameters: map[string]string{"badparam": "foo"},
+			},
+			TestResult: testutils.ExpectError(http.StatusBadRequest),
+		},
+		testutils.TestCase{Ctx: handlerCtx,
+			Request: events.APIGatewayProxyRequest{
+				HTTPMethod: http.MethodGet,
+			},
+			TestResult: testutils.ExpectError(http.StatusBadRequest, "No network requested, please add query parameters"),
+		},
 	}
-
-	for _, c := range cases {
-		t.Logf("Testing query: %v", c.queryParameters)
-		response, err := Handler(handlerCtx, events.APIGatewayProxyRequest{QueryStringParameters: c.queryParameters, HTTPMethod: http.MethodGet})
-		if err != nil {
-			t.Errorf("error occurred while testing handler: %v", err)
-			continue
-		}
-
-		status := response.StatusCode
-		if status != c.ExpectedStatus {
-			t.Errorf("Expected status %d, got %d", c.ExpectedStatus, status)
-		}
-
-		switch c.ExpectedBody.(type) {
-		case lambdautils.ErrorResponse:
-			body := lambdautils.ErrorResponse{}
-			err = json.Unmarshal([]byte(response.Body), &body)
-			if err != nil {
-				t.Errorf("Unable to unmarshal error in response: %v", err)
-			}
-
-			if diff := deep.Equal(body, c.ExpectedBody); len(diff) > 0 {
-				t.Errorf("body doesn't match expected:")
-				for _, l := range diff {
-					t.Errorf(l)
-				}
-			}
-
-		case *inventorytypes.Network:
-			body := &inventorytypes.Network{}
-			err = json.Unmarshal([]byte(response.Body), body)
-			if err != nil {
-				t.Errorf("Unable to unmarshal network in response: %v", err)
-			}
-
-			if diff := deep.Equal(body, c.ExpectedBody); len(diff) > 0 {
-				t.Errorf("body doesn't match expected:")
-				for _, l := range diff {
-					t.Errorf(l)
-				}
-			}
-		default:
-			t.Errorf("You've specified a return type that isn't implemented for testing.")
-		}
-	}
-
+	cases.RunTests(t, Handler)
 }
